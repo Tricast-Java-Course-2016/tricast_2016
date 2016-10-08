ab510a4a289aad44645044e269d49212da69b3bd
totitma <barthatamas87@gmail.com>
2016-10-08 19:41:53 -0200

    Create Market

--- src/main/java/com/tricast/beans/Market.java
+++ src/main/java/com/tricast/beans/Market.java
@@ -2,4 +2,35 @@ package com.tricast.beans;
 
 public class Market {
 
+	private long id;
+	private long periodld;
+	private long marketTypeId;
+	private String description;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getPeriodld() {
+		return periodld;
+	}
+	public void setPeriodld(long periodld) {
+		this.periodld = periodld;
+	}
+	public long getMarketTypeId() {
+		return marketTypeId;
+	}
+	public void setMarketTypeId(long marketTypeId) {
+		this.marketTypeId = marketTypeId;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 }
