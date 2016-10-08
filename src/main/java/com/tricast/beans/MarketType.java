5a1c3c2ea1aa8af1ae053899059e39cd21e073f2
wegh <wegh95@gmail.com>
2016-10-08 20:01:41 +0200

    Fixing MarketType

--- src/main/java/com/tricast/beans/MarketType.java
+++ src/main/java/com/tricast/beans/MarketType.java
@@ -5,11 +5,19 @@ public enum MarketType {
     SINGLE(1), SYSTEM(2);
 
     private long marketTypeId;
+    private String marketTypeIdDescription;
 
     private MarketType(long typeId) {
         this.marketTypeId = typeId;
     }
 
+    public String getMarketTypeIdDescription() {
+        return marketTypeIdDescription;
+    }
+
+    public void setMarketTypeIdDescription(String marketTypeIdDescription) {
+        this.marketTypeIdDescription = marketTypeIdDescription;
+    }
     public long getTypeId() {
         return marketTypeId;
     }
