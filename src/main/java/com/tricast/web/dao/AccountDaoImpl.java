90859efbaf7c9990ea4ad1b940124080cf8e7986
Kiss Gabor <kissgabor92@gmail.com>
2016-10-19 15:08:44 +0200

    team1: Account dao Imp v2

--- src/main/java/com/tricast/web/dao/AccountDaoImpl.java
+++ src/main/java/com/tricast/web/dao/AccountDaoImpl.java
@@ -15,14 +15,12 @@ import com.tricast.database.Workspace;
 import com.tricast.web.annotations.JdbcTransaction;
 
 public class AccountDaoImpl implements AccountDao {
-
     private static final SqlManager sqlManager = SqlManager.getInstance();
     private static final org.apache.log4j.Logger logger = org.apache.log4j.LogManager.getLogger(CountryDao.class);
 
     @Override
     @JdbcTransaction
     public List<Account> getAll(Workspace workspace) throws SQLException, IOException {
-
         List<Account> result = new ArrayList<Account>();
         String sql = sqlManager.get("accountsGetAll.sql");
 
@@ -38,10 +36,8 @@ public class AccountDaoImpl implements AccountDao {
     }
 
     private Account buildAccount(ResultSet rs, boolean isLogin) throws SQLException {
-
         Account account = new Account();
         int i = 1;
-
         account.setId(rs.getLong(i++));
         account.setAccountTypeId(rs.getLong(i++));
         account.setUserName(rs.getString(i++));
@@ -65,7 +61,6 @@ public class AccountDaoImpl implements AccountDao {
     @Override
     @JdbcTransaction
     public Account getById(Workspace workspace, long id) throws SQLException, IOException {
-
         Account result = null;
         ResultSet rs = null;
         String sql = sqlManager.get("accountGetById.sql");
@@ -73,6 +68,7 @@ public class AccountDaoImpl implements AccountDao {
         try (PreparedStatement ps = workspace.getPreparedStatement(sql)) {
             ps.setLong(1, id);
             rs = ps.executeQuery();
+
             if (rs.next()) {
                 result = buildAccount(rs, true);
             }
@@ -88,10 +84,8 @@ public class AccountDaoImpl implements AccountDao {
     @Override
     @JdbcTransaction
     public Account login(Workspace workspace, String username, String password) throws SQLException, IOException {
-
         Account result = null;
         ResultSet rs = null;
-
         String sql = sqlManager.get("accountLogin.sql");
 
         try (PreparedStatement ps = workspace.getPreparedStatement(sql)) {
@@ -115,11 +109,10 @@ public class AccountDaoImpl implements AccountDao {
     @Override
     @JdbcTransaction
     public Long create(Workspace workspace, Account newItem) throws SQLException, IOException {
-
 		Long result = null;
 		ResultSet rs = null;
-
 		String sql = sqlManager.get("accountCreate.sql");
+
 		try (PreparedStatement ps = workspace.getPreparedStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 			int i = 1;
 			ps.setLong(i++, newItem.getAccountTypeId());
@@ -154,7 +147,6 @@ public class AccountDaoImpl implements AccountDao {
     @Override
     @JdbcTransaction
     public Long update(Workspace workspace, Account updateItem) throws SQLException, IOException {
-
         Long result = null;
         ResultSet rs = null;
         String sql = sqlManager.get("accountUpdate.sql");
@@ -190,9 +182,8 @@ public class AccountDaoImpl implements AccountDao {
     @Override
     @JdbcTransaction
     public boolean deleteById(Workspace workspace, long Id) throws SQLException, IOException {
-
         boolean result = false;
-	String sql = sqlManager.get("accountDeleteById.sql");
+        String sql = sqlManager.get("accountDeleteById.sql");
 
         try (PreparedStatement ps = workspace.getPreparedStatement(sql)) {
             int i = 1;
