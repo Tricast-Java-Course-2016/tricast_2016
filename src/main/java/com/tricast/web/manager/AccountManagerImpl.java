07232ce9dfab830a6213f8ca4f09e7e94bf5aaa5
Kiss Gabor <kissgabor92@gmail.com>
2016-10-19 19:38:17 +0200

    team1: Account Managers and impl

--- src/main/java/com/tricast/web/manager/AccountManagerImpl.java
+++ src/main/java/com/tricast/web/manager/AccountManagerImpl.java
@@ -8,29 +8,32 @@ import javax.inject.Inject;
 
 import com.tricast.beans.Account;
 import com.tricast.database.Workspace;
+import com.tricast.web.annotations.JdbcTransaction;
 import com.tricast.web.dao.AccountDao;
 
 public class AccountManagerImpl implements AccountManager {
-
 	private final AccountDao accountDao;
+
 	@Inject
     public AccountManagerImpl(AccountDao accountDao) {
 		this.accountDao = accountDao;
 	}
 
     @Override
+    @JdbcTransaction
     public List<Account> getAll(Workspace workspace) throws SQLException, IOException {
-        // TODO Auto-generated method stub
-        return null;
+        return accountDao.getAll(workspace);
     }
 
     @Override
+    @JdbcTransaction
     public Account getById(Workspace workspace, long id) throws SQLException, IOException {
-        // TODO Auto-generated method stub
-        return null;
+        Account account = accountDao.getById(workspace, id);
+        return account;
     }
 
     @Override
+    @JdbcTransaction
     public Account create(Workspace workspace, Account newAccount) throws SQLException, IOException {
     	Long id = accountDao.create(workspace, newAccount);
 		if (id != null) {
@@ -41,23 +44,29 @@ public class AccountManagerImpl implements AccountManager {
     }
 
     @Override
+    @JdbcTransaction
     public Account update(Workspace workspace, Account updateAccount) throws SQLException, IOException {
-        // TODO Auto-generated method stub
-        return null;
+        Long id = accountDao.update(workspace, updateAccount);
+        if (id != null) {
+            return accountDao.getById(workspace, id);
+        } else {
+            return null;
+        }
     }
 
     @Override
+    @JdbcTransaction
     public boolean deleteById(Workspace workspace, long Id) throws SQLException, IOException {
-        // TODO Auto-generated method stub
-        return false;
+        return accountDao.deleteById(workspace, Id);
     }
 
     @Override
+    @JdbcTransaction
     public Account login(Workspace workspace, String username, String password) throws SQLException, IOException {
-        // TODO Auto-generated method stub
-        return null;
+        Account account = accountDao.login(workspace, username, password);
+        if (account == null) {
+            throw new SQLException("No account exists with the specified username:" + username);
+        }
+        return account;
     }
-
-
-
 }
