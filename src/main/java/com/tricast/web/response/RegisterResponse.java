d4516b697a61c8d8eee58fb15df7a25075dc37b7
Kiss Gabor <kissgabor92@gmail.com>
2016-11-01 21:32:55 +0100

    team1 response/request házi
    added
    -RegisterResponse

---
+++ src/main/java/com/tricast/web/response/RegisterResponse.java
@@ -0,0 +1,18 @@
+package com.tricast.web.response;
+
+public class RegisterResponse {
+
+    private long accountId;
+    private long accountTypeId;
+    private String accountUserName;
+    private String accountPassword;
+    private String accountFirstName;
+    private String accountLastName;
+    private String accountDOB;
+    private String accountAddress;
+    private String accountEmailAddress;
+    private String accountPhoneNumber;
+    private String accountPIN;
+    private String accountBankAccountNumber;
+    private String accountBankCardNumber;
+}
