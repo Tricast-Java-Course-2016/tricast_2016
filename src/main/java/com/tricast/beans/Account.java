d1f9bb341fb240336b03ef9480e5017417fbf717
Zoli5 <szuri90@gmail.com>
2016-11-02 19:59:29 +0100

    Team 1
    Re-uploaded source files which were overwritten by other team.

--- src/main/java/com/tricast/beans/Account.java
+++ src/main/java/com/tricast/beans/Account.java
@@ -1,6 +1,6 @@
 package com.tricast.beans;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class Account {
 	
--- src/main/java/com/tricast/web/server/AccountService.java
+++ src/main/java/com/tricast/web/server/AccountService.java
@@ -7,8 +7,10 @@ import java.sql.SQLException;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -73,6 +75,32 @@ public class AccountService extends LVSResource {
 			return respondPost(ex.getMessage(), "\\accounts", 500);
 		}
 	}
+	
+	@PUT
+	@Produces(APPLICATION_JSON)
+	@Consumes(APPLICATION_JSON)
+	public Response updateAccount(Account updateAccount) throws SQLException, OutOfTransactionException, IOException {
+		log.trace("Trying to update account for " + updateAccount.getFirstName() + ' ' + updateAccount.getLastName());
+		try {
+			return respondPut(manager.update(workspace, updateAccount));
+		} catch (SQLException ex) {
+			return respondPut(ex.getMessage(), 500);
+		}
+	}
+	
+	@DELETE
+    @Path("{id}")
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
+    public Response deleteOutcome(@PathParam("id") long id)
+            throws SQLException, OutOfTransactionException, IOException {
+        log.trace("Trying to delete user with id #" + id);
+        try {
+            return respondDelete(manager.deleteById(workspace,id));
+        } catch (SQLException ex) {
+            return respondDeleteNotOK(ex.getMessage(), null, 500);
+        }
+    }
 
     @POST
     @Path("/login")
