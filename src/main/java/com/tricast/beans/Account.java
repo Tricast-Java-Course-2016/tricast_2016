abbea449a40fff344f10192c9a9b3bd879c03ea9
Gabor <Gabor@WIN7-PC>
2016-11-11 17:54:39 +0100

    team1: partial register fix +accountCreate serverside time /zoli

--- src/main/java/com/tricast/beans/Account.java
+++ src/main/java/com/tricast/beans/Account.java
@@ -4,28 +4,28 @@ import java.sql.Date;
 
 public class Account {
 
-	private long id;
+    private long id;
     private AccountType type;
-	private String userName;
-	private String password;
-	private String firstName;
-	private String lastName;
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
     private String dob;
-	private String address;
-	private String emailAddress;
-	private String phoneNumber;
+    private String address;
+    private String emailAddress;
+    private String phoneNumber;
     private String pin;
-	private String bankAccountNumber;
-	private String bankCardNumber;
-	private Date createdDate;
+    private String bankAccountNumber;
+    private String bankCardNumber;
+    private Date createdDate;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public AccountType getType() {
         return type;
@@ -36,98 +36,98 @@ public class Account {
     }
 
     public String getUserName() {
-		return userName;
-	}
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getdob() {
         return dob;
-	}
+    }
 
     public void setdob(String dob) {
         this.dob = dob;
-	}
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public String getEmailAddress() {
-		return emailAddress;
-	}
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getpin() {
         return pin;
-	}
+    }
 
     public void setpin(String pin) {
         this.pin = pin;
-	}
+    }
 
-	public String getBankAccountNumber() {
-		return bankAccountNumber;
-	}
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
 
-	public void setBankAccountNumber(String bankAccountNumber) {
-		this.bankAccountNumber = bankAccountNumber;
-	}
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
 
-	public String getBankCardNumber() {
-		return bankCardNumber;
-	}
+    public String getBankCardNumber() {
+        return bankCardNumber;
+    }
 
-	public void setBankCardNumber(String bankCardNumber) {
-		this.bankCardNumber = bankCardNumber;
-	}
+    public void setBankCardNumber(String bankCardNumber) {
+        this.bankCardNumber = bankCardNumber;
+    }
 
-	public Date getCreatedDate() {
-		return createdDate;
-	}
+    public Date getCreatedDate() {
+        return createdDate;
+    }
 
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+    // public void setCreatedDate(Date createdDate) {
+    // this.createdDate = createdDate;
+    // }
 }
