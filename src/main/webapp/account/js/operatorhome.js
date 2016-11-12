c8e184aead3bf89f56efb538e5c9e7170ed8c669
Mark <v.mark94@hotmail.com>
2016-11-12 15:06:56 +0100

    Creating eventsoperator stuffs

--- src/main/webapp/account/js/operatorhome.js
+++ src/main/webapp/account/js/operatorhome.js
@@ -3,23 +3,15 @@ $(document).ready(function() {
     assignAction();
 });
 
-function assignAction() {
-    
-    function goToChangeAccountInfo(){
-        window.location.href = "/tricast-2016-sportsbook/account/change.html";
-    }
-    function goToEventList(){
-        
-    }
-    function goToPeriods(){
-        
-    }
-    function goToTeams(){
-        
-    }
-    
+function goToChangeAccountInfo() {
+    window.location.href = "/tricast-2016-sportsbook/account/change.html";
+}
+function goToEventList() {
+    window.location.href = "/tricast-2016-sportsbook/eventsoperator/eventsoperator.html";
+}
+function goToPeriods() {
 
-    });
-    
+}
+function goToTeams() {
 
 }
