eed5d72965c548126547f2522dccc61c3abe80ae
barth <barth@DESKTOP-3OQ97OE>
2016-11-12 19:48:54 -0200

    webGetTeam

---
+++ src/main/webapp/team/js/addTeam.js
@@ -0,0 +1,8 @@
+//this method runs every time when the page is reloading
+$(document).ready(function() {
+    assignAction();
+});
+
+function assignAction() {
+    var teams = createTeam();
+}
---
+++ src/main/webapp/team/js/teams.js
@@ -0,0 +1,31 @@
+//this method runs every time when the page is reloading
+$(document).ready(function() {
+    assignAction();
+});
+
+function assignAction() {
+    var teams = getAllTeams();
+}
+
+function getAllTeams() {
+
+    var url = "/tricast-2016-sportsbook/services/leagues";
+    var leagues = new Map();
+
+    sendAjax("GET", url, null, function(data, textStatus, xhr) {
+        for (var i = 0; i < data.length; i++) {
+            leagues.set(data[i].id, data[i].description);
+            
+            $('#teamTable > tbody:last-child').append(
+                    '<tr><td>' +data[i].id+
+                    '</td><td>' +data[i].description+
+            		'</td></tr>');
+        }
+
+    }, function(xhr) {
+        var errormsg = getErrorMsg(xhr);
+        alert(errormsg);
+    });
+
+    return teams;
+}
